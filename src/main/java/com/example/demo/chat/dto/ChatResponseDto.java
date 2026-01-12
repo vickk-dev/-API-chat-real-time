96280b536ac9c297e7eb35609d765f5d07b4a806
vickk-dev <victor.160612-2023@aluno.unicv.edu.br>
2026-01-12 20:49:28 -0300

    :contruction:<fix>(ChatResponseDto) Acrescentado mais dois campos de resposta, um para o e-mail do destino e outro para a data

--- src/main/java/com/example/demo/chat/dto/ChatResponseDto.java
+++ src/main/java/com/example/demo/chat/dto/ChatResponseDto.java
@@ -1,19 +1,22 @@
 package com.example.demo.chat.dto;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 
 import com.example.demo.user.dto.ResponseUserDto;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@AllArgsConstructor
-@NoArgsConstructor
 @Data
+@NoArgsConstructor
 public class ChatResponseDto {
 	private String id;
 	private String displayName;
 	private List<ResponseUserDto> participants;
-	
+	private String receiverEmail;
+	private LocalDateTime createdAt;
+
 }
